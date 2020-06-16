f98a386755dda2f3374901a19132401e9e94e687
Marta <marta.marta.k@gmail.com>
2020-06-16 09:10:48 +0200

    rendering Tours and accessing it's data on TourList

--- src/components/TourList/TourList.js
+++ src/components/TourList/TourList.js
@@ -2,14 +2,26 @@ import React, { Component } from 'react';
 //because it is set as main in package.json it goes to Tour.js
 import Tour from '../Tour';
 import './TourList.scss';
+import {tourData} from '../../tourData';
 
 export default class index extends Component {
+  state = {
+    tours: tourData
+  }
+
+  removeTour = id =>{
+    console.log(id);
+    
+  }
   render() {
+    // console.log(this.state.tours);
+    const {tours} = this.state;
     return (
-      <div>
-        Hi from TourList
-        <Tour />
-      </div>
+      <section className="tourList">
+        {
+          tours.map(tour => (<Tour key={tour.id} tour={tour} removeTour={this.removeTour} ></Tour>))
+        }
+      </section>
     )
   }
 }
